b307705018d04672b613fd2687c05060f11b20dc
Luiz-Cunha-Dev <luizcunha.dev99@gmail.com>
2022-09-26 13:55:01 -0300

    input funcionando

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import forca6 from "./img/forca6.png";
 import palavras from "./palavras";
 
 
-const letras = ["A", "B", "C","D", "E", "F","G", "H", "I","J", "K", "L","M", "N", "O","P", "Q", "R","S", "T", "U","V", "W", "X","Y", "Z"];
 
 export default function App(){
 
@@ -18,6 +17,10 @@ export default function App(){
     const [letrasPalavraSorteada, setletrasPalavraSorteada]= useState([]);
     const [selecionadas, setSelecionadas] = useState([]);
     const [letrasRestantes, setLetrasRestantes] = useState([]);
+    const [contadorErros, setContadorErros] = useState(0);
+    const [contadorAcertos, setContadorAcertos] = useState(0);
+    const [palavraChute, setPalavraChute] = useState("");
+    const imagens = [forca0, forca1, forca2, forca3, forca4, forca5, forca6]
 
 
     function sortearPalavra(array){
@@ -34,52 +37,118 @@ export default function App(){
         setletrasPalavraSorteada(letrasPalavra);
         setSelecionadas([]);
         setLetrasRestantes(arrayDetraços);
+        setContadorAcertos(0);
+        setContadorErros(0);
+        setPalavraChute("");
     }
 
 
     function selecionarLetra(index){
-        if(!selecionadas.includes(index)){
+        if(!selecionadas.includes(index) && letrasRestantes.length !== 0){
             let novaArray = [...selecionadas, index];
             setSelecionadas(novaArray)
-            console.log(novaArray);
             mostraLetra(letras[index])
         }
 
     }
 
     function mostraLetra(letra){
-        let letrasAcertadas = [...letrasRestantes]
-        for(let i = 0; i < letrasPalavraSorteada.length; i++){
+        let letrasAcertadas = [...letrasRestantes];
+        let contadorDeAcertos = 0;
+        let contadorDeErros = -1*(letrasPalavraSorteada.length);
+
+        if(contadorAcertos < letrasPalavraSorteada.length){
+            for(let i = 0; i < letrasPalavraSorteada.length; i++){
             if(letra.toLowerCase() === letrasPalavraSorteada[i].normalize("NFD").replace(/[\u0300-\u036f]/g, "")){
                 letrasAcertadas[i] = letrasPalavraSorteada[i];
-                setLetrasRestantes(letrasAcertadas)
+                setLetrasRestantes(letrasAcertadas);
+                contadorDeAcertos++
+            }else{
+                contadorDeErros++
             }
         }
+        setContadorAcertos(contadorAcertos + contadorDeAcertos)
+        if(contadorDeErros === 0){
+            if(contadorErros !== 6){
+                setContadorErros(contadorErros + 1)
+            }
+
+        }}
+    }
+
+    function chutarPalavra(){
+       let arrayPalavraChute = [];
+       let arrayPalavra= [];
+       let contador = 0;
+
+       if(contadorAcertos < letrasPalavraSorteada.length && contadorErros < 6){
+        for(let i = 0; i < palavraChute.length; i++){
+            arrayPalavraChute.push(palavraChute[i])
+        }
+
+        for(let i = 0; i < letrasPalavraSorteada.length; i++){
+            arrayPalavra.push(letrasPalavraSorteada[i].normalize("NFD").replace(/[\u0300-\u036f]/g, ""))
+        }
+
         
+        for(let i = 0; i < letrasPalavraSorteada.length; i++){
+
+            if(palavraChute[i] === undefined){         
+            }
+            else if(palavraChute[i].normalize("NFD").replace(/[\u0300-\u036f]/g, "") === arrayPalavra[i]){
+                contador++
+            }
+
+            if(contador === letrasPalavraSorteada.length){
+                setLetrasRestantes(letrasPalavraSorteada);
+                setContadorAcertos(letrasPalavraSorteada.length)
+                setContadorErros(contadorErros);
+            }else{
+                setContadorErros(6);
+            }
+        }
+
+}
     }
 
 
     return(
         <div className="container">
         <div className="principal">
-            <img src={forca0} />    
+            <img data-identifier={`game-image`} src={imagens[contadorErros]} />    
             <div className="direita">
-                <button onClick={() => sortearPalavra(palavras)}>Escolher Palavra</button>
-                <div className="palavra">
-                    {letrasRestantes.map((l, index) => <div key={index} className="letra">{l}</div> )}
+                <button data-identifier="choose-word" onClick={() => sortearPalavra(palavras)}>Escolher Palavra</button>
+                <div className={`palavra 
+                ${(contadorErros === 6) ? "vermelho" : ""}
+                 ${(contadorAcertos === letrasPalavraSorteada.length) ? "verde" : ""}`}>
+                    {(contadorErros === 6 ? letrasPalavraSorteada : letrasRestantes).map((l, index) => <div data-identifier={`word`} key={index} className="letra">{l}</div> )}
         
                 </div>
             </div>
         </div>
         <div className="letras">
-            {letras.map((l, index) => <div key={index} onClick={() => selecionarLetra(index)}
-             className={`bloco-letra ${selecionadas.includes(index) ? "selecionado" : "" }`}
-            >{l}</div>)}
+            {letras.map((l, index) => <div  data-identifier={`letter`} key={index}  onClick={() => selecionarLetra(index)}
+             className={`bloco-letra 
+             ${selecionadas.includes(index) ? "selecionado" : "" }
+             ${(letrasRestantes.length === 0) ? "selecionado" : "" }
+             ${contadorErros === 6 ? "selecionado" : ""}
+             ${contadorAcertos === letrasPalavraSorteada.length ? "selecionado" : ""}
+             `}>{l}</div>)}
         </div>
-        <div className="chutar">
+        <div className="chutar" >
+
             <span>Já sei a palavra!</span>
-            <input type="text"/>
-            <button>Chutar</button>
+            <input type="text"
+            data-identifier={`type-guess`}
+            value={palavraChute}
+            onChange={e => setPalavraChute(e.target.value)}
+            />
+            <button data-identifier="guess-button" className={` 
+             ${(letrasRestantes.length === 0) ? "botao-selecionado" : "botao-chutar"}
+             ${contadorErros === 6 ? "botao-selecionado" : "botao-chutar"}
+             ${contadorAcertos === letrasPalavraSorteada.length ? "botao-selecionado" : "botao-chutar"}
+             `}
+              onClick={chutarPalavra}>Chutar</button>
         </div>
         </div>
     )
